c0db18de5a728a37ec89d321a079b27cf476ea24
adenkesuma <adenfdfd@gmail.com>
2023-07-21 12:03:14 +0700

    add detail company location

--- components/jobdetails/company/Company.jsx
+++ components/jobdetails/company/Company.jsx
@@ -24,6 +24,22 @@ const Company = ({
           style={styles.logoImage}
         />
       </View>
+
+      <View style={styles.jobTitleBox}>
+        <Text style={styles.jobTitle}>{jobTitle}</Text>
+      </View>
+
+      <View style={styles.companyInfoBox}>
+          <Text style={styles.companyName}>{companyName} / </Text>
+          <View style={styles.locationBox}>
+            <Image 
+              source={icons.location}
+              resizeMode="contain"
+              style={styles.locationImage}
+            />
+            <Text style={styles.locationName}>{location}</Text>
+          </View>
+      </View>
     </View>
   );
 };
